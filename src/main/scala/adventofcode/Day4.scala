e442ea096ab844f3556bb697c3cffca7b2ad1775
Kirill Kulikov <kirill.kulikov@gmail.com>
2018-12-10 00:12:54 +0000

    day 4 - code clean up

--- src/main/scala/adventofcode/Day4.scala
+++ src/main/scala/adventofcode/Day4.scala
@@ -1,8 +1,5 @@
 package adventofcode
 
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-
 import scala.collection.mutable
 import scala.io.Source
 
@@ -10,86 +7,74 @@ object Day4 extends App /*with AdventPartOne*/ {
 
   case class Sleep(start: Int, end: Option[Int])
 
-  case class Guard(id: Int, sleep: Seq[Sleep] = Seq.empty[Sleep])
+  case class Event(id: Int, sleep: Seq[Sleep] = Seq.empty[Sleep])
+
+  case class Id(id: Int) extends AnyVal
 
   def partOne(lines: Iterable[String]): Int = {
 
-    val timestamp = """\[([\d\-]+\s\d\d\:\d\d)]\s.*""".r("timestamp")
-    val beginsShift = """\[[\d\-\s\:]+]\sGuard\s\#(\d+)\sbegins shift""".r("id")
-    val fallsAsleep = """\[[\d\-\s]+\:(\d\d)]\sfalls asleep""".r("min")
-    val wakesUp = """\[[\d\-\s]+\:(\d\d)]\swakes up""".r("min")
+    val guards = guardsWithTimes(parseEvents(lines))
 
-    implicit val localDateOrdering: Ordering[LocalDateTime] = _ compareTo _
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
-    val sorted = lines.toList.sortBy(s => LocalDateTime.parse(timestamp.findAllIn(s).group("timestamp"), formatter))
+    val guard = guards.mapValues(_.map(p => p._2 - p._1).sum).toList.maxBy(_._2)._1
 
-    val guards = sorted.foldLeft(Seq.empty[Guard]) {
-      case (z, l) if beginsShift.findFirstIn(l).nonEmpty =>
-        val id = beginsShift.findAllIn(l).group("id").toInt
-        Guard(id) +: z
-      case (h :: z, l) if fallsAsleep.findFirstIn(l).nonEmpty =>
-        val min = fallsAsleep.findAllIn(l).group("min").toInt
-        h.copy(sleep = Sleep(min, None) +: h.sleep) +: z
-      case (h :: z, l) if wakesUp.findFirstIn(l).nonEmpty =>
-        val min = wakesUp.findAllIn(l).group("min").toInt
-        h.copy(sleep = h.sleep.head.copy(end = Some(min)) +: h.sleep.tail) +: z
-      case _ =>
-        throw new UnsupportedOperationException("ERROR")
-    }
-
-    val times = guards.groupBy(_.id).mapValues(_.flatMap(_.sleep).map {
-      case Sleep(s, Some(e)) => (s, e)
-      case _ => throw new UnsupportedOperationException("ERROR")
-    })
-
-    val guard = times.mapValues(_.map(p => p._2 - p._1).sum).toList.maxBy(_._2)._1
-
-    val minute = times(guard).foldLeft(mutable.Map.empty[Int, Int].withDefaultValue(0)) { case (m, p) =>
+    val minute = guards(guard).foldLeft(mutable.Map.empty[Int, Int].withDefaultValue(0)) { case (m, p) =>
       for (key <- p._1 until p._2) {
         m(key) = m(key) + 1
       }
       m
     }.maxBy(p => p._2)._1
 
-    guard * minute
+    guard.id * minute
   }
 
   def partTwo(lines: Iterable[String]): Int = {
 
-    val timestamp = """\[([\d\-]+\s\d\d\:\d\d)]\s.*""".r("timestamp")
+    val guards = guardsWithTimes(parseEvents(lines))
+
+    val agg = (0 until 60).map(minute =>
+      (minute, guards.mapValues(q => q.count(c => c._1 <= minute && minute < c._2)))).toMap
+
+    val result = agg.mapValues(_.maxBy(_._2)).maxBy(_._2._2)
+
+    result._2._1.id * result._1
+  }
+
+  private def guardsWithTimes(events: Seq[Event]) = {
+    events.groupBy(_.id).mapValues(_.flatMap(_.sleep).map {
+      case Sleep(s, Some(e)) => (s, e)
+      case _ => throw new UnsupportedOperationException("ERROR")
+    }).map(q => (Id(q._1), q._2))
+  }
+
+  private def parseEvents(lines: Iterable[String]) = {
+
     val beginsShift = """\[[\d\-\s\:]+]\sGuard\s\#(\d+)\sbegins shift""".r("id")
+
+    def beginsShiftMatch(line: String) = beginsShift.findAllIn(line)
+
     val fallsAsleep = """\[[\d\-\s]+\:(\d\d)]\sfalls asleep""".r("min")
+
+    def fallsAsleepMatch(line: String) = fallsAsleep.findAllIn(line)
+
     val wakesUp = """\[[\d\-\s]+\:(\d\d)]\swakes up""".r("min")
 
-    implicit val localDateOrdering: Ordering[LocalDateTime] = _ compareTo _
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
-    val sorted = lines.toList.sortBy(s => LocalDateTime.parse(timestamp.findAllIn(s).group("timestamp"), formatter))
+    def wakesUpMatch(line: String) = wakesUp.findAllIn(line)
 
-    val guards = sorted.foldLeft(Seq.empty[Guard]) {
-      case (z, l) if beginsShift.findFirstIn(l).nonEmpty =>
-        val id = beginsShift.findAllIn(l).group("id").toInt
-        Guard(id) +: z
-      case (h :: z, l) if fallsAsleep.findFirstIn(l).nonEmpty =>
-        val min = fallsAsleep.findAllIn(l).group("min").toInt
+    val sorted = lines.toList.sorted // just a string ordering
+
+    sorted.foldLeft(Seq.empty[Event]) {
+      case (z, line) if beginsShiftMatch(line).hasNext =>
+        val id = beginsShiftMatch(line).group("id").toInt
+        Event(id) +: z
+      case (h :: z, line) if fallsAsleepMatch(line).hasNext =>
+        val min = fallsAsleepMatch(line).group("min").toInt
         h.copy(sleep = Sleep(min, None) +: h.sleep) +: z
-      case (h :: z, l) if wakesUp.findFirstIn(l).nonEmpty =>
-        val min = wakesUp.findAllIn(l).group("min").toInt
+      case (h :: z, line) if wakesUpMatch(line).hasNext =>
+        val min = wakesUpMatch(line).group("min").toInt
         h.copy(sleep = h.sleep.head.copy(end = Some(min)) +: h.sleep.tail) +: z
       case _ =>
         throw new UnsupportedOperationException("ERROR")
     }
-
-    val times = guards.groupBy(_.id).mapValues(_.flatMap(_.sleep).map {
-      case Sleep(s, Some(e)) => (s, e)
-      case _ => throw new UnsupportedOperationException("ERROR")
-    })
-
-    val agg = (0 until 60).map(minute =>
-      (minute, times.mapValues(q => q.count(c => c._1 <= minute && minute < c._2)))).toMap
-
-    val result = agg.mapValues(_.maxBy(_._2)).maxBy(_._2._2)
-
-    result._1 * result._2._1
   }
 
   val input = Source.fromResource("day4input.txt").getLines().toStream
